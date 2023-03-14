1029a91538f3b6dfc06ba1a4ae17bb4a085164dd
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2023-03-14 15:56:37 +0000

    Update help text

--- src/structs.jl
+++ src/structs.jl
@@ -17,18 +17,50 @@ function Base.String(iterator::NLLSIterator)
 end
 
 struct NLLSOptions
-    dcost::Float64
-    dstep::Float64
-    maxfails::Int
-    maxiters::Int
-    iterator::NLLSIterator
-    linearsolver
-    callback
-    storecosts::Bool
-    storetrajectory::Bool
+    dcost::Float64              # Minimum relative reduction in cost required to avoid termination
+    dstep::Float64              # Minimum L-infinity norm of the update vector required to avoid termination
+    maxfails::Int               # Maximum number of consecutive iterations that have a higher cost than the current best before termination
+    maxiters::Int               # Maximum number of outer iterations
+    iterator::NLLSIterator      # Inner iterator (see above for options)
+    callback                    # Callback called every outer iteration - returns true to terminate
+    storecosts::Bool            # Indicates whether the cost per outer iteration should be stored
+    storetrajectory::Bool       # Indicates whether the step per outer iteration should be stored
 end
-function NLLSOptions(; maxiters=100, dcost=1.e-6, dstep=1.e-6, maxfails=3, iterator=gaussnewton, callback=(args...)->false, storecosts=false, storetrajectory=false, linearsolver=nothing)
-    NLLSOptions(dcost, dstep, maxfails, maxiters, iterator, linearsolver, callback, storecosts, storetrajectory)
+function NLLSOptions(; maxiters=100, dcost=1.e-6, dstep=1.e-6, maxfails=3, iterator=gaussnewton, callback=(args...)->false, storecosts=false, storetrajectory=false)
+    NLLSOptions(dcost, dstep, maxfails, maxiters, iterator, callback, storecosts, storetrajectory)
+end
+
+struct NLLSResult
+    startcost::Float64                      # The function cost prior to minimization
+    bestcost::Float64                       # The lowest function cost achieved
+    timetotal::Float64                      # The time (in seconds) to run the optimization (excluding initialization)
+    timeinit::Float64                       # The time (in seconds) to initialize the internal data structures
+    timecost::Float64                       # Time (in seconds) spent computing the cost
+    timegradient::Float64                   # Time (in seconds) spent computing the residual gradients and constructing the linear problems
+    timesolver::Float64                     # Time (in seconds) spent solving the linear problems
+    niterations::Int                        # Number of outer optimization iterations performed
+    costcomputations::Int                   # Number of cost computations performed
+    gradientcomputations::Int               # Number of residual gradient computations performed
+    linearsolvers::Int                      # Number of linear solves performed
+    costs::Vector{Float64}                  # Vector of costs at the end of each outer iteration
+    trajectory::Vector{Vector{Float64}}     # Vector of update vectors at the end of each outer iteration
+end
+
+function Base.show(io::IO, x::NLLSResult)
+    timetotal = x.timetotal + x.timeinit
+    otherstuff = x.timetotal - x.timecost - x.timegradient - x.timesolver
+    @printf(io, "NLLSsolver optimization took %f seconds and %d iterations to reduce the cost from %f to %f (a %.2f%% reduction), using:
+   %d cost computations in %f seconds (%.2f%% of total time),
+   %d gradient computations in %f seconds (%.2f%% of total time),
+   %d linear solver computations in %f seconds (%.2f%% of total time),
+   %f seconds for initialization (%.2f%% of total time), and
+   %f seconds for other stuff (%.2f%% of total time).\n", 
+            timetotal, x.niterations, x.startcost, x.bestcost, 100*(1-x.bestcost/x.startcost), 
+            x.costcomputations, x.timecost, 100*x.timecost/timetotal,
+            x.gradientcomputations, x.timegradient, 100*x.timegradient/timetotal,
+            x.linearsolvers, x.timesolver, 100*x.timesolver/timetotal,
+            x.timeinit, 100*x.timeinit/timetotal,
+            otherstuff, 100*otherstuff/timetotal)
 end
 
 mutable struct NLLSInternal{VarTypes}
@@ -78,37 +110,4 @@ mutable struct NLLSInternal{VarTypes}
 end
 function NLLSInternal(problem::NLLSProblem{VarTypes}, computehessian) where VarTypes
     return NLLSInternal{VarTypes}(problem, computehessian)
-end
-
-struct NLLSResult
-    startcost::Float64
-    bestcost::Float64
-    timetotal::Float64
-    timeinit::Float64
-    timecost::Float64
-    timegradient::Float64
-    timesolver::Float64
-    niterations::Int
-    costcomputations::Int
-    gradientcomputations::Int
-    linearsolvers::Int
-    costs::Vector{Float64}
-    trajectory::Vector{Vector{Float64}}
-end
-
-function Base.show(io::IO, x::NLLSResult)
-    timetotal = x.timetotal + x.timeinit
-    otherstuff = x.timetotal - x.timecost - x.timegradient - x.timesolver
-    @printf(io, "NLLSsolver optimization took %f seconds and %d iterations to reduce the cost from %f to %f (a %.2f%% reduction), using:
-   %d cost computations in %f seconds (%.2f%% of total time),
-   %d gradient computations in %f seconds (%.2f%% of total time),
-   %d linear solver computations in %f seconds (%.2f%% of total time),
-   %f seconds for initialization (%.2f%% of total time), and
-   %f seconds for other stuff (%.2f%% of total time).\n", 
-            timetotal, x.niterations, x.startcost, x.bestcost, 100*(1-x.bestcost/x.startcost), 
-            x.costcomputations, x.timecost, 100*x.timecost/timetotal,
-            x.gradientcomputations, x.timegradient, 100*x.timegradient/timetotal,
-            x.linearsolvers, x.timesolver, 100*x.timesolver/timetotal,
-            x.timeinit, 100*x.timeinit/timetotal,
-            otherstuff, 100*otherstuff/timetotal)
 end
