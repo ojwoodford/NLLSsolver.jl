b0a093cfd9fb5c1fdb24532b86d5efeb19947121
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2023-06-02 16:44:39 +0100

    Update variables

--- src/visualgeometry/camera.jl
+++ src/visualgeometry/camera.jl
@@ -34,10 +34,8 @@ struct SimpleCamera{T}
     end
 end
 SimpleCamera(v::T) where T = SimpleCamera{T}(v::T)
-nvars(@objtype(SimpleCamera)) = 1
-function update(var::SimpleCamera, updatevec, start=1)
-    return SimpleCamera(update(var.f, updatevec, start).val)
-end
+nvars(@objtype(SimpleCamera{T})) where T = 1
+update(var::SimpleCamera, updatevec, start=1) = SimpleCamera(update(var.f, updatevec, start).val)
 @inline cameracenter(::SimpleCamera) = 0
 @inline focallength(camera::SimpleCamera) = camera.f.val
 
@@ -49,10 +47,8 @@ struct NoDistortionCamera{T}
 end
 NoDistortionCamera(fx::T, fy::T, cx::T, cy::T) where T = NoDistortionCamera(ZeroToInfScalar(fx), ZeroToInfScalar(fy), EuclideanVector(cx, cy))
 NoDistortionCamera(f, c) = NoDistortionCamera(f[1], f[2], c[1], c[2])
-nvars(@objtype(NoDistortionCamera)) = 4
-function update(var::NoDistortionCamera, updatevec, start=1)
-    return NoDistortionCamera(update(var.fx, updatevec, start), update(var.fy, updatevec, start+1), update(var.c, updatevec, start+2))
-end
+nvars(@objtype(NoDistortionCamera{T})) where T = 4
+update(var::NoDistortionCamera, updatevec, start=1) = NoDistortionCamera(update(var.fx, updatevec, start), update(var.fy, updatevec, start+1), update(var.c, updatevec, start+2))
 @inline cameracenter(camera::NoDistortionCamera) = camera.c
 @inline focallength(camera::NoDistortionCamera) = SVector(camera.fx.val, camera.fy.val)
 
@@ -62,10 +58,8 @@ struct EULensDistortion{T}
     beta::ZeroToInfScalar{T}
 end
 EULensDistortion(alpha::T, beta::T) where T = EULensDistortion(ZeroToOneScalar(alpha), ZeroToInfScalar(beta))
-nvars(@objtype(EULensDistortion)) = 2
-function update(var::EULensDistortion, updatevec, start=1)
-    return EULensDistortion(update(var.alpha, updatevec, start), update(var.beta, updatevec, start+1))
-end
+nvars(@objtype(EULensDistortion{T})) where T = 2
+update(var::EULensDistortion, updatevec, start=1) = EULensDistortion(update(var.alpha, updatevec, start), update(var.beta, updatevec, start+1))
 
 function ideal2distorted(lens::EULensDistortion, x)
     z = 1 / (1 + lens.alpha.val * (sqrt(lens.beta.val * (x' * x) + 1) - 1))
@@ -95,11 +89,8 @@ struct ExtendedUnifiedCamera{T<:Number}
     lens::EULensDistortion{T}
 end
 ExtendedUnifiedCamera(f, c, a, b) = ExtendedUnifiedCamera(NoDistortionCamera(f[1], f[2], c[1], c[2]), EULensDistortion(a, b))
-nvars(@objtype(ExtendedUnifiedCamera)) = 6
-function update(var::ExtendedUnifiedCamera, updatevec, start=1)
-    return ExtendedUnifiedCamera(update(var.sensor, updatevec, start),
-                                 update(var.lens, updatevec, start+4))
-end
+nvars(@objtype(ExtendedUnifiedCamera{T})) where T = 6
+update(var::ExtendedUnifiedCamera, updatevec, start=1) = ExtendedUnifiedCamera(update(var.sensor, updatevec, start), update(var.lens, updatevec, start+4))
 
 function ideal2image(camera::ExtendedUnifiedCamera, x)
     return ideal2image(camera.sensor, ideal2distorted(camera.lens, x))
@@ -118,10 +109,8 @@ struct BarrelDistortion{T}
     k1::T
     k2::T
 end
-nvars(@objtype(BarrelDistortion)) = 2
-function update(var::BarrelDistortion, updatevec, start=1)
-    return BarrelDistortion(var.k1 + updatevec[start], var.k2 + updatevec[start+1])
-end
+nvars(@objtype(BarrelDistortion{T})) where T = 2
+update(var::BarrelDistortion, updatevec, start=1) = BarrelDistortion(var.k1 + updatevec[start], var.k2 + updatevec[start+1])
 
 function ideal2distorted(lens::BarrelDistortion, x)
     z = x' * x
@@ -133,15 +122,11 @@ struct LensDistortResidual{T} <: AbstractResidual
     rlinear::T
     rdistort::T
 end
-nvars(@objtype(LensDistortResidual)) = 1 # The residual depends on one variable
-nres(@objtype(LensDistortResidual)) = 1 # The residual vector has length one
+nvars(@objtype(LensDistortResidual{T})) where T = 1 # The residual depends on one variable
+nres(@objtype(LensDistortResidual{T})) where T = 1 # The residual vector has length one
 varindices(::LensDistortResidual) = SVector(1)
-function computeresidual(residual::LensDistortResidual, lens::EULensDistortion)
-    return SVector(residual.rdistort - ideal2distorted(lens, residual.rlinear))
-end
-function getvars(::LensDistortResidual{T}, vars::Vector) where T
-    return (vars[1]::EULensDistortion{T},)
-end
+computeresidual(residual::LensDistortResidual, lens::EULensDistortion) = SVector(residual.rdistort - ideal2distorted(lens, residual.rlinear))
+getvars(::LensDistortResidual{T}, vars::Vector) where T = (vars[1]::EULensDistortion{T},)
 
 function convertlens(tolens, fromlens, halfimsz)
     # Create an optimization problem to convert the lens distortion
--- src/visualgeometry/geometry.jl
+++ src/visualgeometry/geometry.jl
@@ -51,14 +51,9 @@ struct Point3D{T<:Real} <: AbstractPoint3D
 end
 Point3D(x, y, z) = Point3D(SVector{3}(x, y, z))
 Point3D() = Point3D(SVector{3}(0., 0., 0.))
-nvars(@objtype(Point3D)) = 3
-function update(var::Point3D, updatevec, start=1)
-    return Point3D(var.v + updatevec[SR(0, 2) .+ start])
-end
-
-function project(x::Point3D)
-    return SVector(x.v[1], x.v[2]) ./ x.v[3]
-end
+nvars(@objtype(Point3D{T})) where T = 3
+update(var::Point3D, updatevec, start=1) = Point3D(var.v + updatevec[SR(0, 2) .+ start])
+project(x::Point3D) = SVector(x.v[1], x.v[2]) ./ x.v[3]
 
 
 abstract type AbstractRotation3D end
@@ -67,10 +62,8 @@ struct Rotation3DR{T<:Real} <: AbstractRotation3D
 end
 Rotation3DR(x, y, z) = Rotation3DR(rodrigues(x, y, z))
 Rotation3DR() = Rotation3DR(SMatrix{3, 3, Float64}(1., 0., 0., 0., 1., 0., 0., 0., 1.))
-nvars(@objtype(Rotation3DR)) = 3
-function update(var::Rotation3DR, updatevec, start=1)
-    return var * rodrigues(updatevec[start], updatevec[start+1], updatevec[start+2])
-end
+nvars(@objtype(Rotation3DR{T})) where T = 3
+update(var::Rotation3DR, updatevec, start=1) = var * rodrigues(updatevec[start], updatevec[start+1], updatevec[start+2])
 transform(rota::Rotation3DR, rotb::Rotation3DR) = Rotation3DR(rota.m * rotb.m)
 transform(rot::Rotation3DR, point::Point3D) = Point3D(rot.m * point.v)
 
@@ -79,10 +72,8 @@ struct Rotation3DL{T<:Real} <: AbstractRotation3D
 end
 Rotation3DL(x, y, z) = Rotation3DL(rodrigues(x, y, z))
 Rotation3DL() = Rotation3DL(SMatrix{3, 3, Float64}(1., 0., 0., 0., 1., 0., 0., 0., 1.))
-nvars(@objtype(Rotation3DL)) = 3
-function update(var::Rotation3DL, updatevec, start=1)
-    return Rotation3DL(updatevec[start], updatevec[start+1], updatevec[start+2]) * var
-end
+nvars(@objtype(Rotation3DL{T})) where T = 3
+update(var::Rotation3DL, updatevec, start=1) = Rotation3DL(updatevec[start], updatevec[start+1], updatevec[start+2]) * var
 transform(rota::Rotation3DL, rotb::Rotation3DL) = Rotation3DL(rota.m * rotb.m)
 transform(rot::Rotation3DL, point::Point3D) = Point3D(rot.m * point.v)
 
@@ -94,13 +85,9 @@ struct Pose3D{T<:Real} <: AbstractPose3D
 end
 Pose3D(rx, ry, rz, tx, ty, tz) = Pose3D(Rotation3DR(rx, ry, rz), Point3D(tx, ty, tz))
 Pose3D() = Pose3D(Rotation3DR(), Point3D())
-nvars(@objtype(Pose3D)) = 6
-function update(var::Pose3D, updatevec, start=1)
-    return Pose3D(update(var.rot, updatevec, start), update(var.trans, updatevec, start+3))
-end
-function inverse(var::Pose3D)
-    return Pose3D(var.rot', var.rot' * -var.trans)
-end
+nvars(@objtype(Pose3D{T})) where T = 6
+update(var::Pose3D, updatevec, start=1) = Pose3D(update(var.rot, updatevec, start), update(var.trans, updatevec, start+3))
+inverse(var::Pose3D) = Pose3D(var.rot', var.rot' * -var.trans)
 transform(pose::Pose3D, point::Point3D) = Point3D(pose.rot.m * point.v + pose.trans.v)
 
 struct EffPose3D{T<:Real} <: AbstractPose3D
@@ -108,14 +95,10 @@ struct EffPose3D{T<:Real} <: AbstractPose3D
     camcenter::Point3D{T}
 end
 EffPose3D(rx, ry, rz, cx, cy, cz) = EffPose3D(Rotation3DL(rx, ry, rz), Point3D(cx, cy, cz))
-EffPose3D() = Pose3D(Rotation3DL(), Point3D())
-nvars(@objtype(EffPose3D)) = 6
-function update(var::EffPose3D, updatevec, start=1)
-    return EffPose3D(update(var.rot, updatevec, start), update(var.camcenter, updatevec, start+3))
-end
-function inverse(var::EffPose3D)
-    return EffPose3D(var.rot', var.rot * -var.camcenter)
-end
+EffPose3D() = EffPose3D(Rotation3DL(), Point3D())
+nvars(@objtype(EffPose3D{T})) where T = 6
+update(var::EffPose3D, updatevec, start=1) = EffPose3D(update(var.rot, updatevec, start), update(var.camcenter, updatevec, start+3))
+inverse(var::EffPose3D) = EffPose3D(var.rot', var.rot * -var.camcenter)
 transform(pose::EffPose3D, point::Point3D) = Point3D(pose.rot.m * (point.v - pose.camcenter.v))
 
 
@@ -125,13 +108,9 @@ struct UnitPose3D{T<:Real}
 end
 UnitPose3D() = Pose3D(Rotation3DL(), Rotation3DL())
 UnitPose3D((rx, ry, rz, tx, ty, tz)) = Pose3D(Rotation3DL(rx, ry, rz), Rotation3DL()) # Normalize translation and initialize y & z axes
-nvars(@objtype(UnitPose3D)) = 5
-function update(var::UnitPose3D, updatevec, start=1)
-    return UnitPose3D(update(var.rot, updatevec, start), update(var.trans, SVector(0, updatevec[start+3], updatevec[start+4])))
-end
-function inverse(var::UnitPose3D)
-    return Pose3D(var.rot', var.rot' * -var.trans.m[:,1])
-end
+nvars(@objtype(UnitPose3D{T})) where T = 5
+update(var::UnitPose3D, updatevec, start=1) = UnitPose3D(update(var.rot, updatevec, start), update(var.trans, SVector(0, updatevec[start+3], updatevec[start+4])))
+inverse(var::UnitPose3D) = Pose3D(var.rot', var.rot' * -var.trans.m[:,1])
 transform(pose::UnitPose3D, point::Point3D) = Point3D(pose.rot.m * point.v + pose.trans.m[:,1])
 
 # Overload multiplication with transformation
