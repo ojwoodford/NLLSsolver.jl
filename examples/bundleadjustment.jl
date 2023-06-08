91784a91fca2ed6ea579e659afe6d97558a2ca30
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2023-06-08 04:36:48 +0100

    Add some comments

--- examples/bundleadjustment.jl
+++ examples/bundleadjustment.jl
@@ -4,11 +4,11 @@ export optimizeBALproblem
 
 # Description of BAL image, and function to transform a landmark from world coordinates to pixel coordinates
 struct BALImage{T}
-    pose::NLLSsolver.EffPose3D{T}
-    sensor::NLLSsolver.SimpleCamera{T}
-    lens::NLLSsolver.BarrelDistortion{T}
+    pose::NLLSsolver.EffPose3D{T}   # Extrinsic parameters (rotation & translation)
+    sensor::NLLSsolver.SimpleCamera{T} # Intrinsic sensor parameters (just a single focal length)
+    lens::NLLSsolver.BarrelDistortion{T} # Intrinsic lens parameters (k1 & k2 barrel distortion)
 end
-NLLSsolver.nvars(NLLSsolver.@objtype(BALImage{T})) where T = 9
+NLLSsolver.nvars(NLLSsolver.@objtype(BALImage{T})) where T = 9 # 9 DoF in total for the image (6 extrinsic, 3 intrinsic)
 function NLLSsolver.update(var::BALImage, updatevec, start=1)
     return BALImage(NLLSsolver.update(var.pose, updatevec, start),
                     NLLSsolver.update(var.sensor, updatevec, start+6),
@@ -21,14 +21,12 @@ function BALImage(rx::T, ry::T, rz::T, tx::T, ty::T, tz::T, f::T, k1::T, k2::T) 
                        NLLSsolver.BarrelDistortion(k1, k2))
 end
 BALImage(v) = BALImage(v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8], v[9])
-function transform(im::BALImage, X::NLLSsolver.Point3D)
-    return NLLSsolver.ideal2image(im.sensor, NLLSsolver.ideal2distorted(im.lens, -NLLSsolver.project(im.pose * X)))
-end
+transform(im::BALImage, X::NLLSsolver.Point3D) = NLLSsolver.ideal2image(im.sensor, NLLSsolver.ideal2distorted(im.lens, -NLLSsolver.project(im.pose * X)))
 
 # Residual that defines the reprojection error of a BAL measurement
 struct BALResidual{T} <: NLLSsolver.AbstractResidual
-    measurement::SVector{2, T}
-    varind::SVector{2, Int}
+    measurement::SVector{2, T} # Landmark image coordinates (x, y)
+    varind::SVector{2, Int} # Variable indices for the residual (image, landmark)
 end
 BALResidual(m, v) = BALResidual(SVector{2}(m[1], m[2]), SVector{2, Int}(v[1], v[2]))
 NLLSsolver.nvars(NLLSsolver.@objtype(BALResidual)) = 2 # Residual depends on 2 variables
