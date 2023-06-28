e56ff55d420ab0c18a35f8da3164bf7a29dbd897
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2023-06-28 08:56:32 -0700

    Make overloading clear

--- src/VectorRepo.jl
+++ src/VectorRepo.jl
@@ -1,5 +1,3 @@
-import Base: values, get, get!, push!, append!, sum
-
 struct VectorRepo{T}
     data::Dict{DataType, Vector}
     function VectorRepo{T}() where T
@@ -8,25 +6,25 @@ struct VectorRepo{T}
 end
 VectorRepo() = VectorRepo{Any}()
 
-@inline values(vr::VectorRepo) = values(vr.data)
+@inline Base.values(vr::VectorRepo) = values(vr.data)
 
-function get(vr::VectorRepo{T}, type::DataType) where T
+function Base.get(vr::VectorRepo{T}, type::DataType) where T
     @assert type<:T "Invalid type"
     return get(vr.data, type, Vector{type}())::Vector{type}
 end
 
-function get!(vr::VectorRepo{T}, type::DataType) where T
+function Base.get!(vr::VectorRepo{T}, type::DataType) where T
     @assert type<:T "Invalid type"
     return get!(vr.data, type, Vector{type}())::Vector{type}
 end
 
-@inline push!(vr::VectorRepo, v::T) where T = push!(get!(vr, T), v)
-@inline append!(vr::VectorRepo, v::Vector{T}) where T = append!(get!(vr, T), v)
+@inline Base.push!(vr::VectorRepo, v::T) where T = push!(get!(vr, T), v)
+@inline Base.append!(vr::VectorRepo, v::Vector{T}) where T = append!(get!(vr, T), v)
 
 # Dynamic dispatch if types are not known 
-@inline sum(fun, vr::VectorRepo{Any}) = sum(Base.Fix1(vrsum, fun), values(vr.data); init=0.0)
+@inline Base.sum(fun, vr::VectorRepo{Any}) = sum(Base.Fix1(vrsum, fun), values(vr.data); init=0.0)
 @inline vrsum(fun, v::Vector) = sum(fun, v; init=0.0)
 # Static dispatch if types are known
-@inline sum(fun, vr::VectorRepo{T}) where T = vrsum(fun, vr, T)
+@inline Base.sum(fun, vr::VectorRepo{T}) where T = vrsum(fun, vr, T)
 @inline vrsum(fun, vr::VectorRepo, T::Union) = vrsum(fun, vr, T.a) + vrsum(fun, vr, T.b)
 @inline vrsum(fun, vr::VectorRepo, T::DataType) = sum(fun, get(vr, T); init=0.0)
