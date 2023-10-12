20b39ac1407de72a5838ee05db7fcfceb939e5c0
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2023-10-12 10:51:41 +0100

    Update decision to use sparse matrix

--- src/utils.jl
+++ src/utils.jl
@@ -105,7 +105,7 @@ function fast_bAb(A::SparseMatrixCSC, b::Vector)
     return total
 end
 
-sparse_dense_decision(d, nnz) = nnz < (d - 40) * d
+sparse_dense_decision(d, nnz) = (nnz * 32) < (25 * d * (d - 40)) # Threshold nnz = 25/32 * (d^2 - 40d)
 
 function block_sparse_nnz(sparsity, blocksizes)
     # Compute the number of non-zeros in a block sparse matrix
