6392baac9fea659e228b024b4e277e4a213ee4ea
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2026-05-06 12:59:25 +0100

    Tighten test

--- test/multithreading.jl
+++ test/multithreading.jl
@@ -3,22 +3,22 @@ import NLLSsolver
 
 @testset "multithreading.jl" begin
     # Cost sums
-    fun(x) = x * x - x
-    v = randn(10000)
+    fun(x) = x * x * x
+    v = rand(1000000)
     s1 = sum(fun, v)
     s2 = NLLSsolver.multithreadedsum(fun, v)
-    @test isapprox(s1, s2; rtol=1.e-12)
+    @test isapprox(s1, s2; rtol=1.e-13)
     # Subset sums with Vector{Bool}
     subset = rand(Bool, length(v))
     s1 = sum(fun(v[i]) for i in eachindex(v) if subset[i])
     s2 = NLLSsolver.sumsubsetloop(fun, subset, v)
-    @test isapprox(s1, s2; rtol=1.e-12)
+    @test isapprox(s1, s2; rtol=1.e-13)
     # Subset sums with BitVector
     subset = BitVector(subset)
     s2 = NLLSsolver.sumsubsetloop(fun, subset, v)
-    @test isapprox(s1, s2; rtol=1.e-12)
+    @test isapprox(s1, s2; rtol=1.e-13)
     # Subset sums with Vector{Int}
     subset = findall(subset)
     s2 = NLLSsolver.sumsubsetloop(fun, subset, v)
-    @test isapprox(s1, s2; rtol=1.e-12)
+    @test isapprox(s1, s2; rtol=1.e-13)
 end
