ce1fb57f3d4b05c64d79156395266579b0012cc5
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2026-06-08 21:16:00 +0100

    Update multithreading.jl

--- src/multithreading.jl
+++ src/multithreading.jl
@@ -1,4 +1,6 @@
 using OhMyThreads
 
+# Single threaded fallback
 multithreadedsum(fun, vec, init) = sum(fun, vec; init=init)
 
+multithreadedsum(fun::Bind{typeof(computecost), A}, vec, init) where A = tmapreduce(fun, +, vec; scheduler=:static, init=init)
