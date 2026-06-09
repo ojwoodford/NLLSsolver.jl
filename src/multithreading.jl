0d375d5f44cf40bd2830f1560314d7ab6ed48572
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2026-06-10 00:12:51 +0100

    Lower multithreading threshold

--- src/multithreading.jl
+++ src/multithreading.jl
@@ -2,7 +2,7 @@ using OhMyThreads
 
 # Overload sum to use multithreading for large vectors
 function Base.sum(fun::Bind{typeof(computecost), Tuple{StaticInt{N}, Vector{T}}}, vec::AbstractVector; init=0.0)::Float64 where {N, T}
-    if N == 1 || length(vec) < Int(1e6)
+    if N == 1 || length(vec) < Int(1e5)
         # Single threaded version
         return sum(Base.Fix1(computecost, fun.args[2]), vec; init=init)
     end
